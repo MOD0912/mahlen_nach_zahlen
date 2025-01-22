6ad3ce3a7b58b3f1e8a1b449b675c1890774bd2a
MOD0912 <melvin.kapferer@gmail.com>
2025-01-22 22:28:21 +0100

    Add random number generator and GUI for car-themed game

---
+++ main.py
@@ -0,0 +1,170 @@
+import customtkinter as ctk
+import random
+
+ctk.deactivate_automatic_dpi_awareness()
+
+class RandomNumberGenerator:
+    def __init__(self):
+        self.operators = ['+', '-']
+    
+    def read_file(self, file):
+        with open(file, 'r') as f:
+            lines = f.readlines()
+        lst = [] 
+        lst2 = [] 
+        lst3 = []
+        for line in lines:
+            if line == "ÃŸ\n":
+                x = lst2.copy()
+                lst.append(x)
+                lst2.clear()
+            else:
+                lst2.append(line)
+                lst2[-1] = lst2[-1].replace("\n", "")
+
+        for i in lst:
+            for j in i:
+                for x in j:
+                    if x not in lst3 and x != "Ã" and x != "Ÿ" and x != "\n":
+                        lst3.append(x)
+
+
+        print(lst3)
+
+        self.dic = {}
+        lst4 = [i for i in range(len(lst3))]
+ 
+
+        for i in lst3:
+            x = random.choice(lst4)
+            lst4.remove(x)
+            self.dic[x] = i
+        for i in lst[0]:
+            print(i)
+        print(self.dic)
+        return self.dic, lst[1]
+        
+    
+    def generate_number(self, button, real_result, x, y):
+        while True:
+            number1 = random.randint(0, x)
+            number2 = random.randint(0, y)
+            operator = random.choice(self.operators)
+            
+            if operator == '+':
+                result = number1 + number2
+            else:
+                result = number1 - number2
+            
+            if result == real_result:
+                return [button, number1, operator, number2]
+            
+            
+            
+class GUI(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Mahlen nach Zahlen")
+        self.car = []
+        
+    def create_buttons(self, x, y, chosen_car, dic, color_dic, multiplier=1):
+        self.x = x
+        self.y = y
+        c_w = [i for i in range(0, self.x)]
+        r_w = [i for i in range(0, self.y)]
+        self.grid_columnconfigure(c_w, weight=1)
+        self.grid_rowconfigure(r_w, weight=1)
+        self.buttons = []
+        self.buttons_1 = []
+        self.geometry(f"{self.x*80}x{self.y*80}")
+        
+     
+        for i in range(0, self.y):
+            for j in range(0, self.x):
+                try: 
+                    color = color_dic[chosen_car[i//1][j//1]]
+                    text = chosen_car[i//1][j//1]
+                except:
+                    color = "grey"
+                    text = " "
+                button = ctk.CTkButton(self, text=text, fg_color="white", text_color=color, corner_radius=10, font=("Arial", 80))
+                button.configure(command=lambda i=button: self.button_click(i))
+                button.grid(row=i%self.y, column=j, pady=5, padx=5, sticky="nsew")
+                self.buttons_1.append(button)
+            self.buttons.append(self.buttons_1)
+        print(i for i in self.buttons)
+    
+
+
+class Application(GUI):
+    def __init__(self):
+        super().__init__()
+        self.update()   
+        
+        self.randomgenerate = RandomNumberGenerator()
+        self.dic, lst = self.randomgenerate.read_file("car.txt")
+        self.color_dic = {
+            "/": "black",
+            "|": "black",
+            "\\": "black",
+            "(": "black",
+            ")": "black",
+            "}": "orange",
+            "{": "pink",
+            "'": "brown",
+            '"': "blue",
+            "[": "red",
+            "]": "red",
+            "=": "black",
+            " ": "grey",
+            "`": "black",
+            "~": "black",
+            "!": "grey",
+            "@": "grey",
+            "#": "grey",
+            "$": "grey",
+            "%": "grey",
+            "^": "grey",
+            "&": "grey",
+            "*": "grey",
+            "_": "black",
+            "+": "grey",
+            "-": "black",
+            "´": "grey",
+            "o": "blue",
+            ".": "pink",
+            "d": "pink",
+            "b": "pink",
+            "<": "gray",
+            ">": "gray",
+        }
+        self.chosen_car = random.choice(lst)
+        self.chosen_car = lst
+        longest = 0
+        print(self.chosen_car)
+        print()
+        print()
+        for i in self.chosen_car:
+            if len(i) > longest:
+                longest = len(i)
+            print(i)
+        height = len(self.chosen_car)
+        multiplier = 1
+        x = longest * multiplier
+        y = height * multiplier
+        print(x, y)
+        
+        self.create_buttons(x, y, self.chosen_car, self.dic, self.color_dic, multiplier)
+        self.updat()
+        
+    def updat(self):
+        while True:
+            self.update()
+    
+    def button_click(self, button):
+            
+        self.dic[button.cget("text")]
+
+        
+if __name__ == "__main__":
+    app = Application()
